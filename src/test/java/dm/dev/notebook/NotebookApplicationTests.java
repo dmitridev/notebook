2096194d14d7e4c2d44228dd7a2b55e2f311aa15
root <root@uhfbmqwvtg.local>
2025-03-26 12:22:19 +0000

    start.spring.io generated code

---
+++ src/test/java/dm/dev/notebook/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package dm.dev.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
